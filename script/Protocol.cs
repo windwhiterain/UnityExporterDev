68933f3d646017d45a8b33ca3a04bdeff244cb06
windwhiterain <1712428442@qq.com>
2023-05-25 09:34:23 +0800

    dataSystem

---
+++ script/Protocol.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+namespace Exporter
+{
+    public class Protocol : MonoBehaviour
+    {
+        [System.Serializable]
+        public class IdNamePair { public int id; public string typeName; }
+        [System.Serializable]
+        public class IdNameMap { public IdNamePair[] pairs; }
+        [SerializeField] IdNameMap idNameMap;
+        [SerializeField] string path;
+        [ContextMenu("Parse")]
+        public void Parse()
+        {
+            string json = File.ReadAllText(path);
+            idNameMap = JsonUtility.FromJson<IdNameMap>(json);
+            indexIdMap = new int[idNameMap.pairs.Length];
+            foreach (var pair in idNameMap.pairs)
+            {
+                indexIdMap[nameIndexMap[pair.typeName]] = pair.id;
+            }
+        }
+        Dictionary<string, int> nameIndexMap = new Dictionary<string, int>()
+    {
+        {"i32",0}
+    };
+        int[] indexIdMap;
+        public IEnumerable<Data> GetDataToDecode(int id)
+        {
+            if (id == indexIdMap[0])
+            {
+                yield return new i32();
+            }
+            else if (id == indexIdMap[1])
+            {
+                yield return new f64();
+            }
+            else if (id == indexIdMap[2])
+            {
+                yield return new ni32();
+            }
+            yield break;
+        }
+    }
+}
--- script/Socket.cs
+++ script/Socket.cs
@@ -2,132 +2,220 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Net.Sockets;  //套接字的命名空间
 using System.Net; //IPAddress的命名空间
 using System.Threading; //线程的命名空间
 
-namespace EasySocket
+namespace Exporter
 {
-    class Connector
+    namespace EasySocket
     {
-        public struct SocketSource : DataSource
+        public class Connector
         {
+            public struct SocketSource : DataSource
+            {
+                Socket socket;
+                public SocketSource(Socket socket)
+                {
+                    this.socket = socket;
+                }
+                public int Read(byte[] buffer, int start, int length)
+                {
+                    return socket.Receive(buffer, start, length, SocketFlags.None);
+                }
+                public int Write(byte[] buffer, int start, int length)
+                {
+                    return socket.Send(buffer, start, length, SocketFlags.None);
+                }
+            }
             Socket socket;
-            public SocketSource(Socket socket)
+            CircularBuffer receive;
+            CircularBuffer send;
+            public Connector(Socket socket)
             {
                 this.socket = socket;
             }
-            public int Read(byte[] buffer, int start, int length)
+            public DataSource Source
             {
-                return socket.Receive(buffer, start, length, SocketFlags.None);
+                get { return new SocketSource(socket); }
             }
-            public int Write(byte[] buffer, int start, int length)
+            DataSourcer toSend = new DataSourcer(true);
+            DataSourcer toReceive = new DataSourcer(false);
+            public void Send(Data data)
             {
-                return socket.Send(buffer, start, length, SocketFlags.None);
+                toSend.Add(data);
             }
-        }
-        Socket socket;
-        CircularBuffer receive;
-        CircularBuffer send;
-        public Connector(Socket socket, int receiveBufferSize, int senBufferSize)
-        {
-            this.socket = socket;
-            this.receive = new CircularBuffer(receiveBufferSize);
-            this.send = new CircularBuffer(senBufferSize);
-        }
-
-        public void UpdateBuffer()
-        {
-            var source = new SocketSource(socket);
-            receive.WriteFromSource(source);
-            send.ReadToSource(source);
-        }
-        public void Close()
-        {
-            socket.Close();
-        }
-    }
-    class Server
-    {
-        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-        IPAddress ip;
-        int port;
-        IPEndPoint ipEndPoint;
-        public Server(string ip, int port)
-        {
-            var _ip = IPAddress.Parse(ip);
-            this.ip = _ip;
-            this.port = port;
-            this.ipEndPoint = new IPEndPoint(_ip, port);
-            socket.Bind(ipEndPoint);
-        }
-        public int maxPendingClients = 1;
-        public int maxClients = 1;
-        public int connectorReceiveBufferSize = 1024;
-        public int connectorSendBufferSize = 1024;
-        List<Connector> clients = new List<Connector>();
-        void FindClient()
-        {
-            socket.Listen(maxPendingClients);
-            while (clients.Count < maxClients)
+            public void Receive(Data data)
             {
-                var newClient = new Connector(socket.Accept(), connectorReceiveBufferSize, connectorSendBufferSize);
-                lock (clients)
+                toReceive.Add(data);
+            }
+            class DataSourcer
+            {
+                bool readOrWrite;
+                public DataSourcer(bool readOrWrite)
                 {
-                    clients.Add(newClient);
+                    this.readOrWrite = readOrWrite;
                 }
-            }
-        }
-        void UpdateConnectorBuffer()
-        {
-            lock (clients)
-            {
-                while (true)
+                Queue<Data> pending = new Queue<Data>();
+                Data onSourcing;
+                Queue<Data> complete = new Queue<Data>();
+                public void Add(Data data) { pending.Enqueue(data); }
+                public Data GetComplete()
                 {
-                    foreach (var connector in clients)
+                    if (complete.Count != 0) { return complete.Dequeue(); }
+                    else return null;
+                }
+                bool NextData()
+                {
+                    if (pending.Count == 0)
                     {
-                        connector.UpdateBuffer();
+                        return false;
+                    }
+                    else
+                    {
+                        onSourcing = pending.Dequeue();
+                        return true;
+                    }
+                }
+                bool UncertainData()
+                {
+                    while (true)
+                    {
+                        if (onSourcing == null || onSourcing.Complete)
+                        {
+                            if (onSourcing != null)
+                            {
+                                complete.Enqueue(onSourcing);
+                            }
+                            if (!NextData()) { return false; }
+                        }
+                        else
+                        {
+                            return true;
+                        }
+                    }
+                }
+                public void Update(DataSource source)
+                {
+                    while (true)
+                    {
+                        if (UncertainData())
+                        {
+                            if (readOrWrite)
+                            {
+                                onSourcing.Read(source);
+                            }
+                            else
+                            {
+                                onSourcing.Write(source);
+                            }
+                        }
+                        if (!onSourcing.Complete) { break; }
+                        if (!NextData()) { break; }
                     }
                 }
             }
-        }
-        Thread findClient;
-        Thread updateConnectorBuffer;
-        bool created = false;
-        public void Create()
-        {
-            if (created) { throw new Exception(); }
-            else { created = true; }
-            findClient = new Thread(FindClient);
-            findClient.Start();
-            updateConnectorBuffer = new Thread(UpdateConnectorBuffer);
-            updateConnectorBuffer.Start();
-        }
-        public void Release()
-        {
-            if (!created) { throw new Exception(); }
-            else { created = false; }
-            findClient.Abort();
-            updateConnectorBuffer.Abort();
-            foreach (var connector in clients)
+
+
+            [ContextMenu("UpdateData")]
+            public void UpdateData()
             {
-                connector.Close();
+                toSend.Update(Source);
+                toReceive.Update(Source);
+
+                var temp = toReceive.GetComplete();
+                if (temp != null)
+                {
+                    Debug.Log("recv");
+                }
             }
-            clients.Clear();
-            socket.Close();
-        }
-        ~Server()
-        {
-            if (created)
+            public void Close()
             {
-                Release();
+                socket.Close();
+            }
+        }
+        class NetNode
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ip;
+            int port;
+            IPEndPoint ipEndPoint;
+            public NetNode(string ip, int port)
+            {
+                var _ip = IPAddress.Parse(ip);
+                this.ip = _ip;
+                this.port = port;
+                this.ipEndPoint = new IPEndPoint(_ip, port);
+                socket.Bind(ipEndPoint);
+            }
+            public int maxPendingClients = 1;
+            public int maxClients = 1;
+            public List<Connector> connectorList = new List<Connector>();
+            void FindClient()
+            {
+                socket.Listen(maxPendingClients);
+                while (connectorList.Count < maxClients)
+                {
+                    var newSocket = new Connector(socket.Accept());
+                    lock (connectorList)
+                    {
+                        connectorList.Add(newSocket);
+                    }
+                }
+            }
+            struct ConnectJob
+            {
+                string ip;
+                int port;
+                NetNode node;
+                public ConnectJob(string ip, int port, NetNode node)
+                {
+                    this.ip = ip;
+                    this.port = port;
+                    this.node = node;
+                    var thread = new Thread(Connect);
+                    thread.Start();
+                }
+                void Connect()
+                {
+                    var newSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    newSocket.Connect(IPAddress.Parse(ip), port);
+                    node.connectorList.Add(new Connector(newSocket));
+                }
+            }
+
+            public void Connect(string ip, int port)
+            {
+                if (!created) { throw new Exception(); }
+                new ConnectJob(ip, port, this);
+            }
+            Thread findClient;
+            bool created = false;
+            public void Create()
+            {
+                if (created) { throw new Exception(); }
+                else { created = true; }
+                findClient = new Thread(FindClient);
+                findClient.Start();
+            }
+            public void Release()
+            {
+                if (!created) { throw new Exception(); }
+                else { created = false; }
+                findClient.Abort();
+                foreach (var connector in connectorList)
+                {
+                    connector.Close();
+                }
+                connectorList.Clear();
+                socket.Close();
+            }
+            ~NetNode()
+            {
+                if (created)
+                {
+                    Release();
+                }
             }
         }
     }
